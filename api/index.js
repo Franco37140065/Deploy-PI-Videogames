870e5cf90a83a158a3ea71cd913d6a19eb2db3cd
franco <franko_millonarios@outlook.es>
2023-03-23 19:03:45 -0300

    deploy

--- api/index.js
+++ api/index.js
@@ -2,8 +2,13 @@ const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 require('dotenv').config();
 const { PORT } = process.env;
+const cors = require('cors')
 
 // Syncing all the models at once.
+const whiteList = ['https://deploy-pi-videogames-git-main-franco37140065.vercel.app','https://deploy-pi-videogames-production-4c06.up.railway.app/']
+server.use (cors({
+  origin:whiteList
+}))
 conn.sync({ force: false }).then(() => {
   server.listen(PORT, () => {
     console.log('%s listening at ',process.env.PORT ); // eslint-disable-line no-console
