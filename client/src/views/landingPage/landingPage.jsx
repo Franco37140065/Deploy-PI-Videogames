ba48e70870663c5d1031a2f30b9840615d77ab27
franco <franko_millonarios@outlook.es>
2023-03-28 12:29:40 -0300

    deploy

--- client/src/views/landingPage/landingPage.jsx
+++ client/src/views/landingPage/landingPage.jsx
@@ -9,9 +9,9 @@ const landingPage =(props)=> {
                     <button className={style.button}>Get into</button>
                 </Link>
             </div> 
-                <div className={style.video}>
+               {/* <div className={style.video}>
                     <iframe width="560" height="315" src="https://www.youtube.com/embed/jwVmF38E85s" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-                </div>
+    </div>*/}
         </div>   
         )
 }
