c29d396b6b925c8609aa622c294791e028dee7fd
franco <franko_millonarios@outlook.es>
2023-03-28 10:33:29 -0300

    deploy

--- api/index.js
+++ api/index.js
@@ -3,7 +3,7 @@ const { conn } = require('./src/db.js');
 require('dotenv').config();
 const { PORT } = process.env;
 
-conn.sync({ force: false }).then(() => {
+conn.sync({ force: true }).then(() => {
   server.listen(PORT, () => {
     console.log('%s listening at ',process.env.PORT ); // eslint-disable-line no-console
   });
